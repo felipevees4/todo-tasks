d09b36578e72de3b98edd869f0abf0d7079d4fd0
Felipe Velasquez <pipevees82@gmail.com>
2022-12-01 21:03:39 -0500

    Variables de entorno

--- api.js
+++ api.js
@@ -2,7 +2,24 @@ var mongoose = require("mongoose");
 var express = require("express");
 var TaskModel = require('./task_schema');
 var router = express.Router();
-var query = "mongodb+srv://felipevelasquez:felipe1234@cluster0.mguygsv.mongodb.net/TaskBD?retryWrites=true&w=majority"
+
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
+
 const db = (query);
 
 mongoose.Promise = global.Promise;
